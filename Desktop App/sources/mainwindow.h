1f1c3efd1569ba7ab03d197bb1897f050ad9eb98
egarciacisneros <105026581+egarciacisneros@users.noreply.github.com>
2022-05-09 10:13:13 -0500

    Create mainwindow.h

---
+++ Desktop App/sources/mainwindow.h
@@ -0,0 +1,35 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtSql>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QDebug>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_insertButton_clicked();
+
+    void on_updatButton_clicked();
+
+    void on_deleteButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QSqlDatabase db;
+    bool connected;
+    int currentID;
+};
+#endif // MAINWINDOW_H
