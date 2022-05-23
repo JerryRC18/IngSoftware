507dfa147d8fa25b3da8332f9e5788270a4d0dc4
egarciacisneros <105026581+egarciacisneros@users.noreply.github.com>
2022-05-22 21:00:11 -0500

    Update mainwindow.cpp

--- Desktop App/sources/mainwindow.cpp
+++ Desktop App/sources/mainwindow.cpp
@@ -6,25 +6,41 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+
+    historial = new Historial();
+
+    db = QSqlDatabase::addDatabase("QMYSQL");
     db.setHostName("127.0.0.1");
     db.setUserName("EduardoGC");
     db.setPassword("+BaalLordOfDestruction1");
     db.setDatabaseName("dentista");
 
-    connected = db.open();
-
-    if (connected) {
+    if (db.open()) {
         QSqlQuery query;
-        if (query.exec("SELECT cit_id, cit_fecha, cit_hora, cit_servicio, usu_id FROM citas JOIN usuarios ON cit_id_usu = usuarios.usu_id JOIN servicios")) {
+        if (query.exec("SELECT cit_id, cit_fecha, cit_hora, cit_servicio, usuarios.usu_nombre FROM citas JOIN usuarios ON cit_id_usu = usuarios.usu_id")) {
             while (query.next()) {
-                currentID = query.value(0).toInt();
-                ui->id->setText(query.value(0).toString());
-                ui->fecha->setText(query.value(1).toString());
-                ui->hora->setText(query.value(2).toString());
-                ui->serv->setText(query.value(3).toString());
-                ui->id_usu->setText(query.value(4).toString());
+               qDebug() << query.value(0) << query.value(1) << query.value(2) << query.value(3) << query.value(4);
+               QString id = query.value(0).toString();
+               QString fecha = query.value(1).toString();
+               QString hora = query.value(2).toString();
+               QString serv =query.value(3).toString();
+               QString usu_id = query.value(4).toString();
+
+               ui->tableWidget->setRowCount(ui->tableWidget->rowCount() + 1);
+               QTableWidgetItem* idItem = new QTableWidgetItem(id);
+               QTableWidgetItem* fechaItem = new QTableWidgetItem(fecha);
+               QTableWidgetItem* horaItem = new QTableWidgetItem(hora);
+               QTableWidgetItem* servItem = new QTableWidgetItem(serv);
+               QTableWidgetItem* usuItem = new QTableWidgetItem(usu_id);
+
+
+               ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 0, idItem);
+               ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 1, fechaItem);
+               ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 2, horaItem);
+               ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 3, servItem);
+               ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 4, usuItem);
             }
+            hasInit = true;
         }
         else {
             qDebug() << query.lastError().text();
@@ -34,6 +50,8 @@ MainWindow::MainWindow(QWidget *parent)
         else {
             qDebug() << "Failed to connect to database.";
     }
+
+
 }
 
 MainWindow::~MainWindow()
@@ -42,77 +60,40 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
-void MainWindow::on_insertButton_clicked()
+void MainWindow::changeWindow()
 {
-    if (connected) {
-        QString id = ui->id->text();
-        QString fecha = ui->fecha->text();
-        QString hora = ui->hora->text();
-        QString id_usu = ui->id_usu->text();
-        QString serv = ui->serv->text();
-        qDebug() << "INSERT INTO citas (cit_id, cit_fecha, cit_hora, cit_id_usu, cit_servicio) VALUES (" + id + ",'"  + fecha + "', '" + hora +"','" + id_usu + "','" + serv + "')";
-        QSqlQuery query;
-        if (query.exec("INSERT INTO citas (cit_id, cit_fecha, cit_hora, cit_id_usu, cit_servicio) VALUES (" + id + ",'"  + fecha + "', '" + hora +"','" + id_usu + "','" + serv + "')")) {
-            currentID = query.lastInsertId().toInt();
-            qDebug() << "Insert success.";
-        } else {
-            qDebug() << query.lastError().text();
-        }
-        } else {
-            qDebug() << "Failed to connect to database.";
-        }
+    if(historial->isVisible())
+    {
+        historial->hide();
+        this->show();
+    }
+    else
+    {
+        this->hide();
+        historial->show();
+    }
 }
 
-void MainWindow::on_updatButton_clicked()
-{
-    if (connected) {
-        if (currentID == 0) {
-            qDebug() << "Nothing to update.";
-        }
-        else {
-            QString id = QString::number(currentID);
-            QString _id = ui->id->text();
-            QString fecha = ui->fecha->text();
-            QString hora = ui->hora->text();
-            QString id_usu = ui->id_usu->text();
-            QString serv = ui->serv->text();
-            qDebug() << "UPDATE citas SET cit_fecha = '" + fecha + "', cit_hora = '" + hora + "', cit_id_usu = '" + id_usu + "', cit_servicio = '" + serv + "' WHERE cit_id = " + _id;
-            QSqlQuery query;
-            if (query.exec("UPDATE citas SET cit_fecha = '" + fecha + "', cit_hora = '" + hora + "', cit_id_usu = '" + id_usu + "', cit_servicio = '" + serv + "' WHERE cit_id = " + _id)) {
-               qDebug() << "Update success.";
-            }
-            else {
-                qDebug() << query.lastError().text();
-            }
-            }
-            } else {
-            qDebug() << "Failed to connect to database.";
-            }
-        }
 
-
-void MainWindow::on_deleteButton_clicked()
+void MainWindow::on_pushButton_clicked()
 {
-    if (connected) {
-        if (currentID == 0) {
-            qDebug() << "Nothing to delete.";
-    }
-    else {
-        QString id = QString::number(currentID);
-        QString _id = ui->id->text();
-        qDebug() << "DELETE FROM citas WHERE cit_id = " + _id;
+    historial->show();
+}
+
+void MainWindow::on_tableWidget_itemDoubleClicked(QTableWidgetItem *item)
+{
+    if(hasInit) {
+        QString id = ui->tableWidget->item(item->row(),0)->data(0).toString();
+        QString fecha = ui->tableWidget->item(item->row(),1)->data(0).toString();
+        QString hora = ui->tableWidget->item(item->row(),2)->data(0).toString();
+
         QSqlQuery query;
-        if (query.exec("DELETE FROM citas WHERE cit_id = " + _id)) {
-            currentID = 0;
-            qDebug() << "Delete success.";
+        if(query.exec("UPDATE citas SET cit_fecha = '" + fecha + "' ,cit_hora = '" + hora +"' WHERE cit_id = " + id)) {
+
         }
         else {
-            qDebug() << query.lastError().text();
+        qDebug() << query.lastError().text();
         }
-    }
-    }
-    else {
-        qDebug() << "Failed to connect to database.";
     }
 }
+
