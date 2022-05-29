d5c1007837cac4663e941faafae6d5a8e0021c45
egarciacisneros <105026581+egarciacisneros@users.noreply.github.com>
2022-05-29 18:23:54 -0500

    Update mainwindow.cpp

--- Desktop App/sources/mainwindow.cpp
+++ Desktop App/sources/mainwindow.cpp
@@ -8,6 +8,7 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     historial = new Historial();
+    update = new class::update();
 
     db = QSqlDatabase::addDatabase("QMYSQL");
     db.setHostName("127.0.0.1");
@@ -17,9 +18,9 @@ MainWindow::MainWindow(QWidget *parent)
 
     if (db.open()) {
         QSqlQuery query;
-        if (query.exec("SELECT cit_id, cit_fecha, cit_hora, cit_servicio, usuarios.usu_nombre FROM citas JOIN usuarios ON cit_id_usu = usuarios.usu_id")) {
+        if (query.exec("SELECT cit_id, cit_fecha, cit_hora, cit_servicio, usuarios.usu_nombre, cit_realizada FROM citas JOIN usuarios ON cit_id_usu = usuarios.usu_id")) {
             while (query.next()) {
-               qDebug() << query.value(0) << query.value(1) << query.value(2) << query.value(3) << query.value(4);
+               qDebug() << query.value(0) << query.value(1) << query.value(2) << query.value(3) << query.value(4) << query.value(5);
                QString id = query.value(0).toString();
                QString fecha = query.value(1).toString();
                QString hora = query.value(2).toString();
@@ -32,6 +33,17 @@ MainWindow::MainWindow(QWidget *parent)
                QTableWidgetItem* horaItem = new QTableWidgetItem(hora);
                QTableWidgetItem* servItem = new QTableWidgetItem(serv);
                QTableWidgetItem* usuItem = new QTableWidgetItem(usu_id);
+               QPushButton* btn = new QPushButton();
+               btn->setText("Delete");
+               connect(btn, &QPushButton::clicked, this, &MainWindow::on_clicked);
+               QPushButton* btn2 = new QPushButton();
+               btn2->setText("Edit");
+
+               QTableWidgetItem* Realizada = new QTableWidgetItem();
+               if(query.value(5) == "\u0001")
+                    Realizada->setCheckState(Qt::Checked);
+               else
+                    Realizada->setCheckState(Qt::Unchecked);
 
 
                ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 0, idItem);
@@ -39,6 +51,9 @@ MainWindow::MainWindow(QWidget *parent)
                ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 2, horaItem);
                ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 3, servItem);
                ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 4, usuItem);
+               ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 5, Realizada);
+               ui->tableWidget->setCellWidget(ui->tableWidget->rowCount() -1, 6, (QWidget*)btn);
+               ui->tableWidget->setCellWidget(ui->tableWidget->rowCount() -1, 7, (QWidget*)btn2);
             }
             hasInit = true;
         }
@@ -50,8 +65,6 @@ MainWindow::MainWindow(QWidget *parent)
         else {
             qDebug() << "Failed to connect to database.";
     }
-
-
 }
 
 MainWindow::~MainWindow()
@@ -72,6 +85,18 @@ void MainWindow::changeWindow()
         this->hide();
         historial->show();
     }
+
+    if(update->isVisible())
+    {
+        update->hide();
+        this->show();
+    }
+    else
+    {
+        this->hide();
+        update->show();
+    }
+
 }
 
 
@@ -94,6 +119,84 @@ void MainWindow::on_tableWidget_itemDoubleClicked(QTableWidgetItem *item)
         else {
         qDebug() << query.lastError().text();
         }
+    }
+}
+
+void MainWindow::on_clicked(){
+
+    QString Id = ui->tableWidget->item(ui->tableWidget->currentRow(), 0)->text();
+    QMessageBox::StandardButton reply = QMessageBox::information(this, "Borrar registro", "Esta seguro de que desea borrar el registro?",QMessageBox::StandardButton::Yes | QMessageBox::StandardButton::No);
+
+    if(reply == QMessageBox::Yes) {
+        qDebug() << ui->tableWidget->item(ui->tableWidget->currentRow(), 0)->text();
+        QSqlQuery query;
+        if (query.exec("DELETE FROM citas WHERE cit_id = " +Id)) {
+            qDebug() << "Deleted";
+        }
+        else {
+        qDebug() << query.lastError().text();
+        }
+    }
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    update->show();
+}
+
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    if (db.open()) {
+        QSqlQuery query;
+        if (query.exec("SELECT cit_id, cit_fecha, cit_hora, cit_servicio, usuarios.usu_nombre, cit_realizada FROM citas JOIN usuarios ON cit_id_usu = usuarios.usu_id")) {
+           ui->tableWidget->setRowCount(0);
+            while (query.next()) {
+               qDebug() << query.value(0) << query.value(1) << query.value(2) << query.value(3) << query.value(4) << query.value(5);
+               QString id = query.value(0).toString();
+               QString fecha = query.value(1).toString();
+               QString hora = query.value(2).toString();
+               QString serv =query.value(3).toString();
+               QString usu_id = query.value(4).toString();
+
+               ui->tableWidget->setRowCount(ui->tableWidget->rowCount() + 1);
+               QTableWidgetItem* idItem = new QTableWidgetItem(id);
+               QTableWidgetItem* fechaItem = new QTableWidgetItem(fecha);
+               QTableWidgetItem* horaItem = new QTableWidgetItem(hora);
+               QTableWidgetItem* servItem = new QTableWidgetItem(serv);
+               QTableWidgetItem* usuItem = new QTableWidgetItem(usu_id);
+               QPushButton* btn = new QPushButton();
+               btn->setText("Delete");
+               connect(btn, &QPushButton::clicked, this, &MainWindow::on_clicked);
+               QPushButton* btn2 = new QPushButton();
+               btn2->setText("Edit");
+
+               QTableWidgetItem* Realizada = new QTableWidgetItem();
+               if(query.value(5) == "\u0001")
+                    Realizada->setCheckState(Qt::Checked);
+               else
+                    Realizada->setCheckState(Qt::Unchecked);
+
+
+               ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 0, idItem);
+               ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 1, fechaItem);
+               ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 2, horaItem);
+               ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 3, servItem);
+               ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 4, usuItem);
+               ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 5, Realizada);
+               ui->tableWidget->setCellWidget(ui->tableWidget->rowCount() -1, 6, (QWidget*)btn);
+               ui->tableWidget->setCellWidget(ui->tableWidget->rowCount() -1, 7, (QWidget*)btn2);
+            }
+            hasInit = true;
+        }
+        else {
+            qDebug() << query.lastError().text();
+        }
+
+    }
+        else {
+            qDebug() << "Failed to connect to database.";
     }
 }
 
