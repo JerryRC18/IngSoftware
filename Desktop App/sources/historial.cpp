83852db3595866b98774dd45a653e04d223a1b51
egarciacisneros <105026581+egarciacisneros@users.noreply.github.com>
2022-06-05 18:24:09 -0500

    Update historial.cpp

--- Desktop App/sources/historial.cpp
+++ Desktop App/sources/historial.cpp
@@ -8,13 +8,14 @@ Historial::Historial(QWidget *parent) :
     ui->setupUi(this);
 
     QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
-    db.setHostName("127.0.0.1");
-    db.setUserName("EduardoGC");
-    db.setPassword("+BaalLordOfDestruction1");
-    db.setDatabaseName("dentista");
+    db.setHostName("bd5s63srxd7fvmbw3cai-mysql.services.clever-cloud.com");
+    db.setUserName("urcwxsqgdun2tukm");
+    db.setPassword("8QDCaE22vYVXmlNxIjNn");
+    db.setDatabaseName("bd5s63srxd7fvmbw3cai");
+    ui->usu_id->addItem("Seleccione el usuario", 0);
     if(db.open()) {
         QSqlQuery query;
-        if (query.exec("SELECT DISTINCT usu_nombre FROM usuarios")) {
+        if (query.exec("SELECT DISTINCT usu_nombre FROM usuarios ORDER BY usu_id")) {
             while (query.next()) {
                 qDebug() << query.value(0);
                 ui->usu_id->addItem(query.value(0).toString(), currentIndex);
@@ -49,15 +50,18 @@ Historial::~Historial()
 
 void Historial::on_accept_clicked()
 {
-        QString fecha = ui->fecha->text();
-        QString hora = ui->hora->text();
+        QString date = ui->date->date().toString(Qt::DateFormat::ISODate);
+        QString time = ui->time->time().toString();
         int usu_id = ui->usu_id->currentIndex();  //currentIndex starts at 0
         QString serv = ui->serv->currentText();
         QString str = QString::number(usu_id);  //For some reason you need to send a String or else you get a syntax error
-        qDebug() << "INSERT INTO citas (cit_fecha, cit_hora, cit_realizada, cit_id_usu, cit_servicio) VALUES ('" +fecha+ "', '" +hora+ "', 0, " +str+ ", '" +serv+ "')";
+        qDebug() << "INSERT INTO citas (cit_fecha, cit_hora, cit_realizada, cit_id_usu, cit_servicio) VALUES ('" +date+ "', '" +time+ "', 0, " +str+ ", '" +serv+ "')";
+
         QSqlQuery query;
-        if (query.exec("INSERT INTO citas (cit_fecha, cit_hora, cit_realizada, cit_id_usu, cit_servicio) VALUES ('" +fecha+ "', '" +hora+ "',0, " +str+ ", '" +serv+ "')")) {
+        if (query.exec("INSERT INTO citas (cit_fecha, cit_hora, cit_realizada, cit_id_usu, cit_servicio) VALUES ('" +date+ "', '" +time+ "',0, " +str+ ", '" +serv+ "')")) {
             qDebug() << "Success";
+            QMessageBox::information(this, "Agendar cita", "Cita agendada correctamente", QMessageBox::StandardButton::Ok);
+            this->close();
         }
         else {
         qDebug() << query.lastError().text();
