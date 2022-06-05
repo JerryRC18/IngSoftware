938aee63c8a338d9830d179e3d3c4a800a27f45a
egarciacisneros <105026581+egarciacisneros@users.noreply.github.com>
2022-06-05 18:25:02 -0500

    Update update.cpp

--- Desktop App/sources/update.cpp
+++ Desktop App/sources/update.cpp
@@ -8,10 +8,12 @@ update::update(QWidget *parent) :
     ui->setupUi(this);
 
     db = QSqlDatabase::addDatabase("QMYSQL");
-    db.setHostName("127.0.0.1");
-    db.setUserName("EduardoGC");
-    db.setPassword("+BaalLordOfDestruction1");
-    db.setDatabaseName("dentista");
+
+    db.setHostName("bd5s63srxd7fvmbw3cai-mysql.services.clever-cloud.com");
+    db.setUserName("urcwxsqgdun2tukm");
+    db.setPassword("8QDCaE22vYVXmlNxIjNn");
+    db.setDatabaseName("bd5s63srxd7fvmbw3cai");
+    ui->paciente->setReadOnly(true);
     if(db.open()) {
         QSqlQuery query;
         if (query.exec("SELECT DISTINCT serv_nombre FROM servicios")) {
@@ -36,8 +38,8 @@ update::~update()
 
 void update::on_actualizar_clicked()
 {
-    QString fecha = ui->fecha->text();
-    QString hora = ui->hora->text();
+    QString date = ui->date->date().toString(Qt::DateFormat::ISODate);
+    QString time = ui->time->time().toString();
     QString id = ui->id->text();
     QString realizada = QString::number(0);
     if(ui->realizada->checkState() == Qt::Checked) {
@@ -45,9 +47,10 @@ void update::on_actualizar_clicked()
     }
 
     QSqlQuery query;
-    qDebug() << "UPDATE citas SET cit_fecha = '" + fecha + "' ,cit_hora = '" + hora +"', cit_realizada = " + realizada + " WHERE cit_id = " + id;
-    if(query.exec("UPDATE citas SET cit_fecha = '" + fecha + "' ,cit_hora = '" + hora +"', cit_realizada = " + realizada + " WHERE cit_id = " + id)) {
+    qDebug() << "UPDATE citas SET cit_fecha = '" + date + "' ,cit_hora = '" + time +"', cit_realizada = " + realizada + " WHERE cit_id = " + id;
+    if(query.exec("UPDATE citas SET cit_fecha = '" + date + "' ,cit_hora = '" + time +"', cit_realizada = " + realizada + " WHERE cit_id = " + id)) {
         QMessageBox::information(this, "Actualizacion", "Cita actualizada", QMessageBox::StandardButton::Ok);
+        this->close();
     }
     else {
     qDebug() << query.lastError().text();
@@ -59,11 +62,13 @@ void update::on_buscar_clicked()
         qDebug() << ui->id->text();
             QSqlQuery query;
             if(query.exec("SELECT usuarios.usu_nombre, cit_fecha, cit_hora, cit_realizada, cit_servicio FROM citas JOIN usuarios ON cit_id_usu = usu_id WHERE cit_id = " +ui->id->text())) {
+                if(query.size() == 0)
+                    QMessageBox::information(this, "Actualizar", "ID no encontrado, refierace a la tabla de citas actualizada", QMessageBox::StandardButton::Ok);
                 while (query.next()) {
                 qDebug() << query.value(0) << query.value(1) << query.value(2);
                 ui->paciente->setText(query.value(0).toString());
-                ui->fecha->setText(query.value(1).toString());
-                ui->hora->setText(query.value(2).toString());
+                ui->date->setDate(query.value(1).toDate());
+                ui->time->setTime(query.value(2).toTime());
                 if(query.value(3) == 1)
                     ui->realizada->setCheckState(Qt::Checked);
                 else
